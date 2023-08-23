9e15a2b875b1f27dae7f06cc7c401a0b5ede93f2
dandrade123 <128077899+dandrade123@users.noreply.github.com>
2023-08-23 09:52:43 -0300

    Update variaveis9.c

--- C/variaveis9.c
+++ C/variaveis9.c
@@ -1,1 +1,15 @@
+#include<stdio.h>
 
+main(){
+
+//variaveis
+float custoFabrica, distribuidor = 0.28, imposto = 0.45, custoFinal;
+
+printf("custo de fabrica");
+scanf("%f", &custoFabrica);
+
+custoFinal = (custoFabrica * distribuidor) * (custoFabrica * imposto) + custoFabrica;
+
+printf("Custo final de veiculo: R$%2.f", custoFinal);
+
+}

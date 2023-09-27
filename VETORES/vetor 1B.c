387be426602697461733fb291fa34169860b76f3
dandrade123 <128077899+dandrade123@users.noreply.github.com>
2023-09-27 09:59:54 -0300

    Add files via upload

---
+++ VETORES/vetor 1B.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+main(){
+
+    int A [6] = (1,0, 5, -2, -5, 7), soma = 0;
+
+    /* soma = A (0) + A (1) + A (5);
+
+    printf("%d", soma); 
+
+    A (4) = 100;
+
+    printf("\n%d", A (0));
+    printf("\n%d", A (1));
+    printf("\n%d", A (2));
+    printf("\n%d", A (3));
+    printf("\n%d", A (4));
+    printf("\n%d", A (5));*/
+
+    for(int i = 0; i < 6; i++ ){
+        if(i == 0 || i == 1 || i == 5){
+            soma += A[1];
+        
+        }
+            if[i == 4]{
+                A[i] = 100;
+            }
+         printf("%d", A[1]); 
+
+    }
+
+    printf("%d", soma); 
+
+}
